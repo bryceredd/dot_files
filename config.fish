1ea48e670b1f2eb42e6ae1a7178f4c29a8764257
Bryce Redd <redd@Bryces-MacBook-Air.local>
2018-04-08 18:53:38 -0600

    osx support

--- config.fish
+++ config.fish
@@ -56,3 +56,10 @@ function fish_prompt --description 'Write out the prompt'
 
     end
 end
+
+# tabtab source for serverless package
+# uninstall by removing these lines or running `tabtab uninstall serverless`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/serverless.fish
+# tabtab source for sls package
+# uninstall by removing these lines or running `tabtab uninstall sls`
+[ -f /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish ]; and . /usr/local/lib/node_modules/serverless/node_modules/tabtab/.completions/sls.fish
